e644bae3f93a50a4d528df4ae08622e7dbf93593
EverSeenTOTOTO <everseenflash@163.com>
2023-05-06 12:02:18 +0800

    test AI CR

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
-import { CacheProvider, EmotionCache } from '@emotion/react';
-import { Route, Routes } from 'react-router-dom';
-import { AppRoutes } from './routes';
-import { AppStore, RootContext } from './store';
+// import { CacheProvider, EmotionCache } from '@emotion/react';
+// import { Route, Routes } from 'react-router-dom';
+// import { AppRoutes } from './routes';
+// import { AppStore, RootContext } from './store';
+//
+// export function App({ store, routes, cache }: { store: AppStore, routes: AppRoutes, cache: EmotionCache }) {
+//   return (
+//     <CacheProvider value={cache}>
+//       <RootContext.Provider value={store}>
+//         <Routes>
+//           {routes.map(({ path, component: RouteComp }) => (
+//             <Route key={path} path={path} element={<RouteComp />} />
+//           ))}
+//         </Routes>
+//       </RootContext.Provider>
+//     </CacheProvider>
+//   );
+// }
 
-export function App({ store, routes, cache }: { store: AppStore, routes: AppRoutes, cache: EmotionCache }) {
-  return (
-    <CacheProvider value={cache}>
-      <RootContext.Provider value={store}>
-        <Routes>
-          {routes.map(({ path, component: RouteComp }) => (
-            <Route key={path} path={path} element={<RouteComp />} />
-          ))}
-        </Routes>
-      </RootContext.Provider>
-    </CacheProvider>
-  );
+export function App() {
+  return <div>Hello World</div>;
 }
